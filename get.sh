23ecd45ba95b54c6a2857bf134709d11d69cac32
Tarsis <br.tmvdl@gmail.com>
2024-03-29 17:15:58 -0300

    Updating app files

--- get.sh
+++ get.sh
@@ -6,4 +6,4 @@ path="${1}"
 
 queries="${2}"
 
-curl -sL -H "Authorization: Client-ID ${ACCESS_KEY}" "https://api.unsplash.com/${path}?${queries}"
+curl -sL -H "Authorization: Client-ID ${ACCESS_KEY}" "https://api.unsplash.com/${path}?${queries}&client_id=${ACCESS_KEY}"
